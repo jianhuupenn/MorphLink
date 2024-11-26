31e2204813f6ca399f9f1bc0b1815132f8d10ff6
jinghuang <109834478+jinghuang-stats@users.noreply.github.com>
2024-11-26 17:21:17 -0500

    Update setup.py

--- MorphLink_package/setup.py
+++ MorphLink_package/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MorphLink", 
-    version="1.0.2",
+    version="1.0.3",
     author="Jing Huang",
     author_email="jing.huang@emory.edu",
     description="MorphLink: Bridging Cell Morphological Behaviors and Molecular Dynamics in Multi-modal Spatial Omics",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jianhuupenn/MorphLink",
     packages=setuptools.find_packages(),
-    install_requires=["numpy","pandas","numba","anndata","scipy","scanpy","scikit-learn","scikit-image","matplotlib","slideio","imutils","opencv-python"],
+    install_requires=["numpy","pandas","numba","anndata","scipy","scanpy","scikit-learn","scikit-image","matplotlib","slideio","imutils","opencv-python","leidenalg"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
